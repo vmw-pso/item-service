33fbe25a55291579c578d7a423e56d551b33db89
vmw-pso <pstacey@vmware.com>
2022-10-27 00:57:17 +1100

    Added activation tokens

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"net/http"
+	"time"
 
 	"github.com/vmx-pso/item-service/internal/data"
 	"github.com/vmx-pso/item-service/internal/validator"
@@ -53,8 +54,19 @@ func (app *application) handleRegisterUser() http.HandlerFunc {
 			return
 		}
 
+		token, err := app.models.Tokens.New(user.ID, 3*24*time.Hour, data.ScopeActivation)
+		if err != nil {
+			app.serverErrorResponse(w, r, err)
+			return
+		}
+
 		app.background(func() {
-			err = app.mailer.Send(user.Email, "user_welcome.tmpl", user)
+			data := map[string]interface{}{
+				"activationToken": token.Plaintext,
+				"userID":          user.ID,
+			}
+
+			err = app.mailer.Send(user.Email, "user_welcome.tmpl", data)
 			if err != nil {
 				app.logger.PrintError(err, nil)
 			}
--- internal/data/models.go
+++ internal/data/models.go
@@ -11,13 +11,15 @@ var (
 )
 
 type Models struct {
-	Items ItemModel
-	Users UserModel
+	Items  ItemModel
+	Users  UserModel
+	Tokens TokenModel
 }
 
 func NewModels(db *sql.DB) *Models {
 	return &Models{
-		Items: ItemModel{DB: db},
-		Users: UserModel{DB: db},
+		Items:  ItemModel{DB: db},
+		Users:  UserModel{DB: db},
+		Tokens: TokenModel{DB: db},
 	}
 }
